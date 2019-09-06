325119de4312538227dd3996105af7de9955a087
trip98jjj <jjjames@ihug.com.au>
2019-09-06 19:09:57 +1000

    Fixed level collisions
    Player can now walk in grey box level 2

---
+++ DemonSlayer/Source/DemonSlayer/Level2GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Level2GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMONSLAYER_API ALevel2GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
